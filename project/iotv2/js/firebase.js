2a9687a95d1629ca8b25ddc2b1b9294b80b9adad
burakwhocodes <burakwhocodes@gmail.com>
2020-12-20 12:48:11 +0300

    iot version 2 eklendi

---
+++ project/iotv2/js/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBaVWIH5ZH-aHAoq5IDQciN99AOYmqWzTg",
+  authDomain: "akilliguvenliksistemiv2.firebaseapp.com",
+  databaseURL: "https://akilliguvenliksistemiv2-default-rtdb.firebaseio.com",
+  projectId: "akilliguvenliksistemiv2",
+  storageBucket: "akilliguvenliksistemiv2.appspot.com",
+  messagingSenderId: "373550998213",
+  appId: "1:373550998213:web:4c9693220340ae4c7ac6ac"
+};
